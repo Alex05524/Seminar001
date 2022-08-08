1aad3ec6b5f9256d1101a319932b698b7ac0378a
Alexey <pwz-and_10@mail.ru>
2022-08-08 19:08:46 +0600

    Create progmamm Task2(1)

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿int a = 2;
+int b = 3;
+int c = 7;
+int max = 0;
+
+if(a > b && a > c)
+{
+    max = a;
+}
+else if(b > c)
+{
+    max = b;
+}
+else
+{
+    max = c;
+}
+
+Console.WriteLine(max);
+
